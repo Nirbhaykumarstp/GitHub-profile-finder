6c1d0c4f594445249fcc6829d515cc7a9d33551e
Nirbhaykumarstp <nirbhaykumarstp59@gmail.com>
2026-09-16 12:36:57 +0530

    Tested Debouncing

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 const form=document.getElementById("form")
 const submitbtn=document.getElementById("submit")
 const section=document.getElementById("user")
+let userName=document.getElementById("userNameInput")
 section.style.display="none"
 
 const userDetails={
@@ -15,69 +16,108 @@ const userDetails={
   ProfileView:document.getElementById("profile")
 }
 
-submitbtn.addEventListener("click",async (Event)=>{
-  Event.preventDefault()
-  section.style.display="block"
-  let userName=document.getElementById("userNameInput").value.trim()
+async function handleMyEvent(userName)
+{
   let apirule=`https://api.github.com/users/${userName}`
-  let response = await fetch(apirule)
-  let data=await response.json()
-  console.log(data)
-  userDetails.image.src=data.avatar_url
-  userDetails.publicRepo.innerText=data.public_repos
-  userDetails.followers.innerText=data.followers
-  userDetails.following.innerText=data.following
-  userDetails.Name.innerText=data.name
-  userDetails.portfolio.innerText=data.blog
-  userDetails.location.innerText=data.location
-  userDetails.date.innerText=new Date(data.created_at).toLocaleString()
-  userDetails.ProfileView.addEventListener("click",(event)=>{
-    window.open(data.html_url)
-  })
-
-  let repoDetails= async()=>{
-    let repos=await fetch(`https://api.github.com/users/${userName}/repos?sort=pushed&direction=desc&per_page=6`)
-    return repos.json()
-  }
-  let repoData= await repoDetails()
-  console.log(typeof repoData)
-  console.log(repoData)
-  console.log(repoData.length)
-  const repos=document.getElementById("repo-div")
-
-  for(let i=1;i<repoData.length;i++)
+  try
   {
-    let newLi=document.createElement("li")
-    newLi.id=`List-${i}`
-    newLi.innerText=repoData[i-1].name
-
-    newLi.addEventListener("click",()=>{
-      window.open(repoData[i-1].html_url)
+    let response = await fetch(apirule)
+    let data= await response.json()  
+    console.log(data)
+    userDetails.image.src=data.avatar_url
+    userDetails.publicRepo.innerText=data.public_repos
+    userDetails.followers.innerText=data.followers
+    userDetails.following.innerText=data.following
+    userDetails.Name.innerText=data.name
+    userDetails.portfolio.innerText=data.blog
+    userDetails.location.innerText=data.location
+    userDetails.date.innerText=new Date(data.created_at).toLocaleString()
+    userDetails.ProfileView.addEventListener("click",(event)=>{
+      window.open(data.html_url)
     })
 
-    let newUl=document.createElement("ul")
-    for(let j=1;j<4;j++)
-    {
-      let anotherLi=document.createElement("li")
-      anotherLi.id=`li-${i}-${j}`
-      if(anotherLi.id==`li-${i}-1`)
-      {
-        anotherLi.innerText=`Stars: ${repoData[i-1].stargazers_count}`
-      }
-      else if(anotherLi.id==`li-${i}-2`)
-      {
-        anotherLi.innerText=`Forks: ${repoData[i-1].forks}`
-      }
-      else if(anotherLi.id==`li-${i}-3`)
-      {
-        anotherLi.innerText=`Language: ${repoData[i-1].language}`
-      }
-      newUl.appendChild(anotherLi)
+    let repoDetails= async()=>{
+      let repos=await fetch(`https://api.github.com/users/${userName}/repos?sort=pushed&direction=desc&per_page=6`)
+      return repos.json()
     }
-    newLi.classList.add("repo-details")
-    newUl.classList.add("repo-detail")
-    repos.appendChild(newLi)
-    newLi.appendChild(newUl)
+    let repoData= await repoDetails()
+    console.log(typeof repoData)
+    console.log(repoData)
+    console.log(repoData.length)
+    const repos=document.getElementById("repo-div")
+
+    for(let i=1;i<repoData.length;i++)
+    {
+      let newLi=document.createElement("li")
+      newLi.id=`List-${i}`
+      newLi.innerText=repoData[i-1].name
+
+      newLi.addEventListener("click",()=>{
+        window.open(repoData[i-1].html_url)
+      })
+
+      let newUl=document.createElement("ul")
+      for(let j=1;j<4;j++)
+      {
+        let anotherLi=document.createElement("li")
+        anotherLi.id=`li-${i}-${j}`
+        if(anotherLi.id==`li-${i}-1`)
+        {
+          anotherLi.innerText=`Stars: ${repoData[i-1].stargazers_count}`
+        }
+        else if(anotherLi.id==`li-${i}-2`)
+        {
+          anotherLi.innerText=`Forks: ${repoData[i-1].forks}`
+        }
+        else if(anotherLi.id==`li-${i}-3`)
+        {
+          anotherLi.innerText=`Language: ${repoData[i-1].language}`
+        }
+        newUl.appendChild(anotherLi)
+      }
+      newLi.classList.add("repo-details")
+      newUl.classList.add("repo-detail")
+      repos.appendChild(newLi)
+      newLi.appendChild(newUl)
+    }
+    console.log(userDetails)
   }
-  console.log(userDetails)
+  catch(Error)
+  {
+    console.log("Something Went Wrong")
+    // let h1=document.createElement("h1").innerText="Something Went Wrong"
+    // document.window.appendChild(h1)
+  }
+}
+
+submitbtn.addEventListener("click",(Event)=>{
+  handleMyEvent(userName.value)
+  section.style.display="block"
+  Event.preventDefault()
 })
+
+//DEBOUNCING-- performance optimization technique, executes the function only after the period of inactivity
+
+// function callingAPIfun()
+// {
+//   handleMyEvent()
+//   section.style.display="block"
+//   document.getElementById("repo-div").innerHTML=""
+// }
+// function debouncing(func,delay)
+// {
+//     let timeoutId=null
+//     return function()
+//     {
+//         clearTimeout(timeoutId)
+//         timeoutId=setTimeout(()=>
+//         {
+//           func()
+//         },delay)
+//     }
+// }
+// const debouncefun=debouncing(callingAPIfun,2000)
+// userName.addEventListener('input',(event)=>{
+    
+//     debouncefun(event.target.value) //1  234567
+// })
